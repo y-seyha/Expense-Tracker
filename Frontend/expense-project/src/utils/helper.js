75fd0c6bad9658fb1ca471967f84609cdaa183e4
seyha yoeun <yoeunseyha11@gmail.com>
2025-12-20 13:25:02 +0700

    implement Summary Card

--- Frontend/expense-project/src/utils/helper.js
+++ Frontend/expense-project/src/utils/helper.js
@@ -15,3 +15,15 @@ export const getInitials = (name) => {
 
   return initials.toLocaleUpperCase();
 };
+
+export const addThousandsSeparator = (num) => {
+  if (num == null || isNaN(num)) return "";
+
+  const [integerPart, fractionalPart] = num.toString().split(".");
+
+  const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  return fractionalPart
+    ? `${formattedInteger}.${fractionalPart}`
+    : formattedInteger;
+};
